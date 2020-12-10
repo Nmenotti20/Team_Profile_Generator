707b8a41cddb4a9fbc48d9a4a20943de05bce1f1
Nickolas Menotti <nmenotti@cox.net>
2020-12-09 20:13:39 -0800

    html

---
+++ Develop/lib/questions.js
@@ -0,0 +1,49 @@
+const questions = [
+
+    // Job Roll
+    {
+     type: 'list',
+     message: '',
+     name: '',
+     choices: '',
+    },
+
+    // Employee Name
+    {
+        type: 'list',
+        message: '',
+        name: '',
+        choices: '',
+       },
+   
+    // Employee ID
+    {
+        type: 'list',
+        message: '',
+        name: '',
+        choices: '',
+       },
+   
+    // Job Roll
+    {
+        type: 'list',
+        message: '',
+        name: '',
+        choices: '',
+       },
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
